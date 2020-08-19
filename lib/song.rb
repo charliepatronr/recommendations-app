3239a98cce12c36609a9fb6787d92a0a703dd4b2
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-08-19 13:00:37 -0500

    Implemented song_viewer method for Song Class

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,10 @@ class Song < ActiveRecord::Base
         end
     end
 
+
+    def self.song_viewer(song_name)
+        song = Song.find_by(name: song_name)
+        puts "#{song.name} by #{song.artist} was recommended by #{song.students.first.name}"
+    end
+
 end
