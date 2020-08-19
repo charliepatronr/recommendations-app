7a3da05473b3a28abb6c2e72e859ac8e4a65d231
charliepatronr <54086538+charliepatronr@users.noreply.github.com>
2020-08-18 20:27:44 -0500

    Code indentation fixed

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,14 +4,14 @@ ActiveRecord::Base.logger = nil
 # Student.login("JoAnna Park")
 
 def greeting
-puts "Welcome to Flatiron Chicago 08032020 Cohort's Playlist!"
+    puts "Welcome to Flatiron Chicago 08032020 Cohort's Playlist!"
 end
 
 def login_or_signup
-puts "Please log in with your username or enter new username to create an account: "
-username = gets.chomp
-$student = Student.find_or_create_by(name: username)
-p "Welcome #{$student.name}!"
+    puts "Please log in with your username or enter new username to create an account: "
+    username = gets.chomp
+    $student = Student.find_or_create_by(name: username)
+    p "Welcome #{$student.name}!"
 end
 
 def menu
@@ -37,7 +37,6 @@ def menu
             genre = gets.chomp
             puts 'Enter the year of the song you would like to recommend: '
             year = gets.chomp.to_i
-
             $student.recommend_song(name, artist, genre, year)
 
         when '2'
@@ -46,34 +45,30 @@ def menu
         when '3'
             my_songs = $student.songs 
             if my_songs.size >= 1
-        p my_songs
-        puts 'Enter the name of the song you would like to edit: '
-        song = gets.chomp
-        puts 'Enter the artist of the song you would like to edit: '
-        artist = gets.chomp
-
-        $student.edit_recommendation(song, artist)
-        
+                p my_songs
+                puts 'Enter the name of the song you would like to edit: '
+                song = gets.chomp
+                puts 'Enter the artist of the song you would like to edit: '
+                artist = gets.chomp
+                $student.edit_recommendation(song, artist)
             else
-            p 'You have no song recommendations!!!'
+                p 'You have no song recommendations!!!'
             end
         when '4'
             my_songs = $student.songs 
             if my_songs.size >= 1
-        p my_songs
-        puts 'Enter the name of the song you would like to delete: '
-        song = gets.chomp
-        puts 'Enter the artist of the song you would like to delete: '
-        artist = gets.chomp
-        $student.delete_recommendation(song, artist)
+                p my_songs
+                puts 'Enter the name of the song you would like to delete: '
+                song = gets.chomp
+                puts 'Enter the artist of the song you would like to delete: '
+                artist = gets.chomp
+                $student.delete_recommendation(song, artist)
             else
-            p 'You have no song recommendations!!!'
+                p 'You have no song recommendations!!!'
             end
-
         when '5'
             puts 'Enter student name: '
             name = gets.chomp
-
             p Student.student_recommendations(name)
     end
 
